bb1b48f912dfabe1a90a87f7e20d10b7a663c89a
Kimser91 <149468829+Kimser91@users.noreply.github.com>
2024-09-10 12:49:05 +0200

    r

--- model.js
+++ model.js
@@ -9,37 +9,37 @@ const model = {
             name: "Pikachu",
             health: 100,
             level: 10,
-            img: "https://pnghq.com/wp-content/uploads/pikachu-pokemon-free-png-images-768x1135.png"
+            img: "pikachu.png"
             },
             {
             name: "Bulbasaur",
             health: 200,
             level: 12,
-            img: "https://archives.bulbagarden.net/media/upload/f/fb/0001Bulbasaur.png"
+            img: "Bulbasaur.png"
             },
             {
             name: "Oranguru",
             health: 120,
             level: 11,
-            img: "https://archives.bulbagarden.net/media/upload/thumb/f/f7/0765Oranguru.png/1024px-0765Oranguru.png"
+            img: "Oranguru.png"
             },
             {
             name: "Drowzee",
             health: 400,
             level: 35,
-            img: "https://archives.bulbagarden.net/media/upload/thumb/e/e4/0096Drowzee.png/250px-0096Drowzee.png"
+            img: "Drowzee.png"
             },
             {
             name: "Charizard",
             health: 300,
             level: 30,
-            img: "https://archives.bulbagarden.net/media/upload/thumb/3/38/0006Charizard.png/1024px-0006Charizard.png"
+            img: "Charizard.png"
             },
             {
             name: "Charmander",
             health: 80,
             level: 3,
-            img: "https://archives.bulbagarden.net/media/upload/thumb/2/27/0004Charmander.png/250px-0004Charmander.png"
+            img: "Charmander.png"
             },
         ],
 
@@ -48,14 +48,14 @@ const model = {
                 name: "Pikachu",
                 health: 100,
                 level: 10,
-                img: "https://pnghq.com/wp-content/uploads/pikachu-pokemon-free-png-images-768x1135.png"
+                img: "pikachu.png"
                 }
         ],
         lastUsedPokemon: { 
             name: "Pikachu",
             health: 100,
             level: 10,
-            img: "https://pnghq.com/wp-content/uploads/pikachu-pokemon-free-png-images-768x1135.png"},
+            img: "pikachu.png"},
         categories: ['grass', 'water', 'fire', 'earth', 'electric', 'psycic'],
         pokeDex: [],
 
